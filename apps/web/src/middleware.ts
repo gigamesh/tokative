2ed28a5ca76e8a4e02ef604dea7ca6c3fe943f29
Matt Masurka <m.masurka@gmail.com>
2026-02-04 22:52:24 -0800

    Trying to fix Clerk integration (middleware proxy blah blah blah)

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse, NextRequest } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -9,15 +10,52 @@ const isPublicRoute = createRouteMatcher([
   "/onboarding",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+function proxyClerkRequests(req: NextRequest) {
+  if (!req.nextUrl.pathname.match("__clerk")) {
+    return null;
+  }
+
+  const proxyHeaders = new Headers(req.headers);
+  proxyHeaders.set(
+    "Clerk-Proxy-Url",
+    process.env.NEXT_PUBLIC_CLERK_PROXY_URL || ""
+  );
+  proxyHeaders.set("Clerk-Secret-Key", process.env.CLERK_SECRET_KEY || "");
+  proxyHeaders.set(
+    "X-Forwarded-For",
+    req.ip || req.headers.get("X-Forwarded-For") || ""
+  );
+
+  const proxyUrl = new URL(req.url);
+  proxyUrl.host = "frontend-api.clerk.dev";
+  proxyUrl.port = "443";
+  proxyUrl.protocol = "https";
+  proxyUrl.pathname = proxyUrl.pathname.replace("/__clerk", "");
+
+  return NextResponse.rewrite(proxyUrl, {
+    request: {
+      headers: proxyHeaders,
+    },
+  });
+}
+
+const clerkHandler = clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth().protect();
   }
 });
 
+export default function middleware(req: NextRequest) {
+  const proxyResponse = proxyClerkRequests(req);
+  if (proxyResponse) {
+    return proxyResponse;
+  }
+  return clerkHandler(req, {} as any);
+}
+
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/(api|trpc|__clerk)(.*)",
   ],
 };
